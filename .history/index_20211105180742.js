a9b7bd5687469e199c994602000d619e54e405f6
minhvt <votuanminh1999@gmail.com>
2021-11-05 20:29:37 +0700

    UL all files

---
+++ .history/index_20211105180742.js
@@ -0,0 +1,149 @@
+"use strict";
+// const ul = document.createElement('ul');
+const str = 'Mozilla';
+let op = ["+", "+", "-", "*", "/"]
+console.log("xoa = ", op.splice());
+console.log(str.substring(1, 3));
+// expected output: "oz"
+
+console.log(str.substring(2));
+// expected output: "zilla"
+// document.body.appendChild(ul);
+var input = document.getElementById('input'), // input/output button
+  number = document.querySelectorAll('.numbers div'), // number buttons
+  operator = document.querySelectorAll('.operators div'), // operator buttons
+  result = document.getElementById('result'), // equal button
+  clear = document.getElementById('clear'), // clear button
+  resultDisplayed = false; // flag to keep an eye on what output is displayed
+console.log(number);
+// adding click handlers to number buttons
+for (var i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function(e) {
+    // alert ( target.innerHTML );
+    // storing current input string and its last character in variables - used later
+    var currentString = input.innerHTML; // node div.input
+    console.log("Numbers-currentString = ",currentString);
+    var lastChar = currentString[currentString.length - 1];
+    console.log("Numbers-lastChar = ", lastChar);
+    // if result is not diplayed, just keep adding
+    if (resultDisplayed === false) {
+      input.innerHTML += e.target.innerHTML;
+      console.log("DISPLAY-e.target.innerHTML = ", input.innerHTML);
+    } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+      // if result is currently displayed and user pressed an operator
+      // we need to keep on adding to the string for next operation
+      resultDisplayed = false;
+      input.innerHTML += e.target.innerHTML;
+    } else {
+      // if result is currently displayed and user pressed a number
+      // we need clear the input string and add the new input to start the new opration
+      resultDisplayed = false;
+      input.innerHTML = "";
+      input.innerHTML += e.target.innerHTML;
+    }
+
+  });
+}
+
+// adding click handlers to number buttons
+for (var i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function(e) {
+    // alert(e.type);
+    // storing current input string and its last character in variables - used later
+    var currentString = input.innerHTML;// 123+1(2) ... khi ma bam + dc 123+12
+    // console.log("currentString-Operator = ", currentString);
+    console.log("Operator currentString = ", currentString);
+    var lastChar = currentString[currentString.length - 1];
+    console.log("Operator lastChar = ", lastChar);
+
+    // if last character entered is an operator, replace it with the currently pressed one //123+ 1
+    if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+        console.log("Ban da nhan nhieu phep Toan lien tuc ***");
+        console.log("currentString[0]", currentString[0]);
+        console.log("currentString[1]", currentString[1]);
+        console.log("currentString[2]", currentString[2]);
+        console.log("currentString[3]", currentString[3]);
+        console.log("currentString[4]", currentString[4]);
+
+
+
+      var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML; //123++
+      input.innerHTML = newString;
+      console.log("newString = ", newString);
+    } else if (currentString.length == 0) {
+      // if first key pressed is an opearator, don't do anything
+      console.log("enter a number first");
+    } else {
+      // else just add the operator pressed to the input
+      input.innerHTML += e.target.innerHTML;
+      console.log("Operator-Else-Display = ", input.innerHTML);
+    }
+
+  });
+}
+
+// on click of 'equal' button
+result.addEventListener("click", function() {
+
+  // this is the string that we will be processing eg. -10+26+33-56*34/23
+  var inputString = input.innerHTML;
+
+  // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
+  var numbers = inputString.split(/\+|\-|\×|\÷/g);
+  console.log("Result-Number = ", numbers);
+
+  // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
+  // first we replace all the numbers and dot with empty string and then split
+  var operators = inputString.replace(/[0-9]|\./g, "").split("");
+  console.log("Result-Operator = ", operators);
+
+  console.log(inputString);
+  console.log(operators);
+  console.log(numbers);
+  console.log("----------------------------");
+
+  // now we are looping through the array and doing one operation at a time.
+  // first divide, then multiply, then subtraction and then addition
+  // as we move we are alterning the original numbers and operators array
+  // the final element remaining in the array will be the output
+
+  var divide = operators.indexOf("÷");
+  while (divide != -1) {
+    numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+    // numbers = ["10", "26", "33", "56", "34", "23"]
+    // operators = ["+", "+", "-", "*", "/"] 36 33 56 34 23
+    operators.splice(divide, 1);
+    divide = operators.indexOf("÷");
+  }
+
+  var multiply = operators.indexOf("×");
+  while (multiply != -1) {
+    numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+    operators.splice(multiply, 1);
+    multiply = operators.indexOf("×");
+  }
+
+  var subtract = operators.indexOf("-");
+  while (subtract != -1) {
+    numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+    operators.splice(subtract, 1);
+    subtract = operators.indexOf("-");
+  }
+
+  var add = operators.indexOf("+");
+  while (add != -1) {
+    // using parseFloat is necessary, otherwise it will result in string concatenation :)
+    numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+    operators.splice(add, 1);
+    add = operators.indexOf("+");
+  }
+
+  input.innerHTML = numbers[0]; // displaying the output
+
+  resultDisplayed = true; // turning flag if result is displayed
+});
+
+// clearing the input on press of clear
+clear.addEventListener("click", function() {
+  input.innerHTML = "";
+})
